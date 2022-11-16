558ee5e74ce98d705383c4a4d81da69247b5c48a
Higor Vital <higor-lopo@hotmail.com>
2022-11-16 18:42:51 -0300

    feat: :art: Criando estrutura
    criando estrutura

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+import Login from './Components/Login/login'
+
+
+function App() {
+  return (
+    <Login></Login>
+  )
+}
+
+export default App
---
+++ vite-project/src/Components/Login/Login.tsx
@@ -0,0 +1,16 @@
+import "../Styles/Login.sass"
+
+export default function Login(){
+    return(
+        <body>
+            <div className="BarraSuperior">
+                <img src="imagens/IconeLivroPequeno.png" alt="ícone de livro pequeno" />
+                <p>BOOKS CORUMBÁ</p>
+            </div>
+
+            <div className="BarraInferior">
+                <p>Contato: email@exemplo.com</p>
+            </div>
+        </body>
+    )
+}
