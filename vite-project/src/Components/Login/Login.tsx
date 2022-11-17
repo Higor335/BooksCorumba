ef74556d2292e67e6699c3089394aac65b6f93fe
Higor Vital <higor-lopo@hotmail.com>
2022-11-17 10:29:43 -0300

    feat: :sparkles: Inserção da tela de login
    inserindo tela de login

--- vite-project/src/Components/Login/Login.tsx
+++ vite-project/src/Components/Login/Login.tsx
@@ -1,4 +1,6 @@
-import "../Styles/Login.sass"
+import "../Styles/Login.scss"
+import { BsTwitter, BsFacebook } from "react-icons/Bs";
+
 
 export default function Login(){
     return(
@@ -6,10 +8,29 @@ export default function Login(){
             <div className="BarraSuperior">
                 <img src="imagens/IconeLivroPequeno.png" alt="ícone de livro pequeno" />
                 <p>BOOKS CORUMBÁ</p>
+                
             </div>
 
             <div className="BarraInferior">
                 <p>Contato: email@exemplo.com</p>
+                <img src="imagens/iconeFacebook.png" alt="facebook icon" />
+                <img src="imagens/Iconetwitter.png" alt="twitter icon" />
+            </div>
+
+            <div className="Form">
+                <h2>LOGIN</h2>
+
+                <form action="">
+                    <label htmlFor="">E-mail: </label>
+                    <input type="text" /><br/>
+                    <label htmlFor="">Senha: </label>
+                    <input type="text" /><br />
+                    <button>Entrar</button>
+                </form>
+                <br />
+                <hr /> 
+                <p>Não Possui uma conta?</p>
+                <button>Cadastrar</button>
             </div>
         </body>
     )
