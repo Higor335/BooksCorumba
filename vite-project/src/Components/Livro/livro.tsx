9847ad5819bd05240bb6123dec68d1c645c9c39b
jose.115 <joseveiga1234@gmail.com>
2022-11-23 16:57:25 -0300

    feat: :sparkles: Adicionando json à tela vendas

---
+++ vite-project/src/Components/Livro/livro.tsx
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom';
+import "../Styles/Vendas.scss"
+
+export default function livro({livro}:any){
+
+    return (
+       
+            <div>
+                <h3>{livro.titulo.substr(0, 10)}</h3>
+                    <div className="texto">
+                        <p>{livro.descricao.substr(0, 260).concat("...")}</p>
+                    </div>
+            </div>
+        
+    )
+    
+}
--- vite-project/src/Components/Vendas/Vendas.tsx
+++ vite-project/src/Components/Vendas/Vendas.tsx
@@ -1,41 +1,54 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import FrameSI from "../FrameSI/FrameSI";
 import "../Styles/Vendas.scss"
+import livros from "../../livros.json"
+import Livro from "../Livro/livro";
 
 export default function Vendas(){
+    const [indice,setIndice] = useState(0);
+    const [indice2,setIndice2] = useState(1);
+    const [indice3,setIndice3] = useState(2);
+
+    function somar(){
+        if(indice3<5){
+            setIndice(indice+3)
+            setIndice2(indice2+3)
+            setIndice3(indice3+3)
+        }
+    }
+    
+    function subtrair(){
+        if(indice>0){
+            setIndice(indice-3)
+            setIndice2(indice2-3)
+            setIndice3(indice3-3)
+        }
+    }
     return (
         <body>
             <div className="conteudo">
                     <div className="blocoLivros">
                         <div className="livro">
-                            <h3>titulo</h3>
-                            <div className="texto">
-                                <p>descrição</p>
-                            </div>
+                            <Livro livro={livros[indice]}/>
+                            <img src="imagens/LivroLoja.png" alt="" />
+                            <Link to={"/Produto"}><button>comprar</button></Link>
+                        </div>
+                        <div className="separador"/>
+                        <div className="livro">                           
+                            <Livro livro={livros[indice2]}/>
                             <img src="imagens/LivroLoja.png" alt="" />
                             <Link to={"/Produto"}><button>comprar</button></Link>
                         </div>
                         <div className="separador"/>
                         <div className="livro">
-                            <h3>titulo</h3>
-                            <div className="texto">
-                                <p>descrição</p>
-                            </div>
-                            <img src="imagens/LivroLoja.png" alt="" />
-                            <Link to={"/Produto"}><button>comprar</button></Link>
-                        </div>
-                        <div className="separador"/>
-                        <div className="livro">
-                            <h3>titulo</h3>
-                            <div className="texto">
-                                <p>descrição</p>
-                            </div>
+                            <Livro livro={livros[indice3]}/>
                             <img src="imagens/LivroLoja.png" alt="" />
                             <Link to={"/Produto"}><button>comprar</button></Link>
                         </div>
                     </div>
-                    <button className="esquerda"><div className="setaEsquerda"></div></button>
-                    <button className="direita"><div className="setaDireita"></div></button>
+                    <button className="esquerda" onClick={subtrair}><div className="setaEsquerda"></div></button>
+                    <button className="direita"  onClick={somar}><div className="setaDireita"></div></button>
             </div>
             <FrameSI/>
         </body>
